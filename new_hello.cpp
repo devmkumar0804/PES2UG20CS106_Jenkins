d8181edc7a8bccb529da8cd9bd91b1a86ba48957
devmkumar0804 <73732254+devmkumar0804@users.noreply.github.com>
2023-02-18 10:46:24 -0500

    Create new_hello.cpp
    task 5 last part

---
+++ new_hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "This is PES2UG20CS106\n";
+  cout << "task 5 \n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
